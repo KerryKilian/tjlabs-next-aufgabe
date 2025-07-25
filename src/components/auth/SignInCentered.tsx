aec32100fa268bc650ab54e5bacfd51c799cf98e
KerryKilian <kilianbrinkner@gmail.com>
2025-07-25 13:42:51 +0200

    feat: card as extra reusable component

--- src/components/auth/SignInCentered.tsx
+++ src/components/auth/SignInCentered.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import Button from '@/components/ui/Button';
+import Card from '@/components/ui/Card';
 import { EyeClosed, Eye } from 'lucide-react';
 
 export default function SignInCentered() {
@@ -10,11 +11,9 @@ export default function SignInCentered() {
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
+
   return (
-    // Auth Container - Full screen centering  
-    <div className="min-h-screen flex items-center justify-center px-4">
-      {/* Auth Card - Figma Design Specs */}
-      <div className="w-full max-w-[420px] h-auto rounded-2xl px-6 py-10 md:p-10 bg-white shadow-[0_24px_48px_0_#919EAB29]">
+    <Card>
         <div className="flex flex-col gap-3 pb-8 text-center mx-auto">
           {/* SignIn Heading */}
           <h4 className="font-[family-name:var(--font-barlow)] text-text-primary text-2xl font-semibold leading-9 tracking-normal">
@@ -55,7 +54,7 @@ export default function SignInCentered() {
             />
             <label
               htmlFor="password"
-              className="floating-label text-sm  text-gray-600"
+              className="floating-label text-sm text-gray-600"
             >
               Password
             </label>
@@ -79,7 +78,6 @@ export default function SignInCentered() {
             Sign In
           </Button>
         </form>
-      </div>
-    </div>
+    </Card>
   );
 }
---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+
+interface CardProps {
+  children: ReactNode;
+}
+
+export default function Card({ children }: CardProps) {
+  return (
+    // Auth Container - Full screen centering  
+    <div className="min-h-screen flex items-center justify-center px-4">
+      {/* Auth Card - Figma Design Specs */}
+      <div className="w-full max-w-[420px] h-auto rounded-2xl px-6 py-10 md:p-10 bg-white shadow-[0_24px_48px_0_#919EAB29]">
+        {children}
+      </div>
+    </div>
+  );
+}
