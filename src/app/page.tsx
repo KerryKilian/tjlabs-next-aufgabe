dc41e20c80746e3532de21df3d617fe846217216
KerryKilian <kilianbrinkner@gmail.com>
2025-07-27 20:53:34 +0200

    feat: redirect to signin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div>
-      <h1>Hallo</h1>
-    </div>
-  );
+  const router = useRouter();
+  useEffect(() => {
+    router.replace("/signin");
+  }, [router]);
+  return null;
 }
